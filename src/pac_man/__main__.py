11e541a4ba565f6f9ecffe5d3ed9c8bd11e46235
Mika Meyer <mimeyer@student.42wolfsburg.de>
2026-09-15 18:22:52 +0200

    feat: mazegen and simple test

--- src/pac_man/__main__.py
+++ src/pac_man/__main__.py
@@ -1,60 +1,17 @@
-from .structures import Size
-from .screenbuffer import ScreenBuffer
-from .spritesheet import SpriteSheet
-from .game import Game
-from mazegenerator import MazeGenerator
-from mlx import Mlx
-import os
 
+from . import MazeGenerator, Map, Player, Ghost, Tile
 
-def on_key(key, param):
-    if key == 65307:  # ESC
-        os._exit(0)
+def get_maze(size: tuple[int], seed) -> list[list[int]]:
+    gen = MazeGenerator(
+        size=size,
+        seed=seed
+    )
+    return (gen.maze)
 
-
-def main() -> int:
-    mlx = Mlx()
-    mlx_ptr = mlx.mlx_init()
-    if mlx_ptr is None:
-        return 1
-
-    win_size = Size(1000, 1000)
-    win_ptr = mlx.mlx_new_window(mlx_ptr, win_size.width,
-                                 win_size.height, "My Window")
-    if win_ptr is None:
-        return 1
-
-    rc = mlx.mlx_key_hook(win_ptr, on_key, None)
-    if rc != 0:
-        return 1
-    rc = mlx.mlx_hook(win_ptr, 33, 0, lambda p: os._exit(0), None)
-    if rc != 0:
-        return 1
-
-    spritesheet = SpriteSheet(mlx=mlx, mlx_ptr=mlx_ptr,
-                              file_path="data/spritesheet.xpm")
-
-    screen = ScreenBuffer(mlx=mlx, mlx_ptr=mlx_ptr, win_ptr=win_ptr,
-                          size=win_size, spritesheet=spritesheet)
-
-    mazegen = MazeGenerator()
-
-    # Very slow idk why
-    game = Game(closed_data=mazegen.maze, screen=screen)
-
-    # a = Tile(closed_state=9, screen=screen)
-    # Tile(closed_state=3, screen=screen)
-
-    # a.display(Position(0, 0))
-
-    game.display()
-
-    screen.render()
-
-    mlx.mlx_loop(mlx_ptr)
-
-    return 0
-
+def main():
+    maze = get_maze((15,15), 42)
+    map_array: list[list[Tile]] = Map().create_map(maze)
+    player = Player
 
 if __name__ == "__main__":
     main()
