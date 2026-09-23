b68bfa0c90fba334e9f45e1620b38664265d0b5d
mimeyer <mimeyer@student.42wolfsburg.de>
2026-09-23 19:44:09 +0200

    feat: FSM able to run program; feat: FSM prep for state-based execution order

--- src/pac_man/loop/fsm.py
+++ src/pac_man/loop/fsm.py
@@ -13,34 +13,50 @@ class LoopStates(Enum):
 	DEATH_SCREEN = 2
 	WIN_SCREEN = 3
 	END_GAME = 4
+	NEW_LEVEL = 5
 
 class LoopMachine():
 	state: LoopStates = LoopStates.MAIN_MENU
 
-	def __init__(self):
-		self.game_loop = GameLoop()
+	def __init__(self, config):
+		self.config = config
+		self.game_loop = GameLoop(config=config)
 		self.main_menu = MainMenu()
 		self.death_screen = DeathScreen()
 		self.win_screen = WinScreen()
 
-	def run_gameloop(self):
+	def run_gameloop(self, asset_cache):
 		log = logging.getLogger('PacMan')
 		while True:
 			match self.state:
+
 				case LoopStates.MAIN_MENU:
 					log.debug('Enter MainMenu')
+					self.state = LoopStates.GAME_LOOP
+					continue
 					self.main_menu.run()
+
 				case LoopStates.GAME_LOOP:
 					log.debug('Enter GameLoop')
+					self.game_loop.load_level(asset_cache)
 					self.game_loop.run()
+					self.state = LoopStates.END_GAME
+
 				case LoopStates.DEATH_SCREEN:
 					log.debug('Enter DeathScreen')
 					self.death_screen.run()
+
 				case LoopStates.WIN_SCREEN:
 					log.debug('Enter WinScreen')
 					self.win_screen.run()
+
 				case LoopStates.END_GAME:
+					log.debug('Enter Close')
 					return
+
+				case LoopStates.NEW_LEVEL:
+					log.debug('Enter NewLevel')
+
 
 
 
